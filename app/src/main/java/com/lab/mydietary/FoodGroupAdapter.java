5fc08daac3e479e3262c43534c40ce2cc4794aba
Eshuen Lam <shuen5035@gmail.com>
2019-11-19 20:16:04 +0800

    All Input Except Location Completed

--- app/src/main/java/com/lab/mydietary/FoodGroupAdapter.java
+++ app/src/main/java/com/lab/mydietary/FoodGroupAdapter.java
@@ -6,16 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class FoodGroupAdapter extends RecyclerView.Adapter<FoodGroupAdapter.MyViewHolder> {
     private String[] food_groups;
     private int[] images;
     private EditText food_group_edit;
+    private CardView card_view_main;
     private Dialog dialog;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
@@ -30,11 +33,12 @@ public class FoodGroupAdapter extends RecyclerView.Adapter<FoodGroupAdapter.MyVi
         }
     }
 
-    public FoodGroupAdapter(String[] food_groups, int[] images, EditText text,Dialog dialog)
+    public FoodGroupAdapter(String[] food_groups, int[] images, EditText text,Dialog dialog,CardView card)
     {
         this.dialog = dialog;
         this.food_groups = food_groups;
         this.images = images;
+        card_view_main = card;
         food_group_edit = text;
     }
 
@@ -56,6 +60,11 @@ public class FoodGroupAdapter extends RecyclerView.Adapter<FoodGroupAdapter.MyVi
             @Override
             public void onClick(View v) {
                 food_group_edit.setText(food_groups[index]);
+                ((ImageView)card_view_main.findViewById(R.id.food_group_image_main)).setImageResource(images[index]);
+                FrameLayout.LayoutParams param = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                param.setMargins(0,20,0,20);
+                ((ImageView)card_view_main.findViewById(R.id.food_group_image_main)).setLayoutParams(param);
+                card_view_main.setElevation(5);
                 dialog.dismiss();
             }
         });
--- app/src/main/java/com/lab/mydietary/MainActivity.java
+++ app/src/main/java/com/lab/mydietary/MainActivity.java
@@ -1,24 +1,32 @@
 package com.lab.mydietary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.icu.text.DecimalFormat;
 import android.icu.util.Calendar;
 import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText food_group_edit,date_edit,time_edit;
+    private EditText food_group_edit,date_edit,time_edit,meal_edit;
     private RecyclerView food_group_list;
     private FoodGroupAdapter adapter;
+    private CardView card_view_main;
     private RecyclerView.LayoutManager layoutManager;
 
     @Override
@@ -28,7 +36,15 @@ public class MainActivity extends AppCompatActivity {
         food_group_edit = findViewById(R.id.food_group_edit);
         date_edit = findViewById(R.id.date_edit);
         time_edit = findViewById(R.id.time_edit);
+        meal_edit = findViewById(R.id.meal_edit);
+        card_view_main = findViewById(R.id.card_view_main);
         food_group_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_group_dialog();
+            }
+        });
+        card_view_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 show_group_dialog();
@@ -46,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
                 show_time_dialog();
             }
         });
+        meal_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_meal_dialog();
+            }
+        });
 
     }
 
@@ -61,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         food_group_list.setHasFixedSize(true);
         String[] food_groups = {"Wholemeal and Grains","Nature's Vegetables","Muscle Building Proteins","Colorful Fruits","Delicious Fats"};
         int[] images={R.drawable.grainsgroup,R.drawable.vegegroup,R.drawable.meatgroup,R.drawable.fruitgroup,R.drawable.fatsgroup};
-        adapter = new FoodGroupAdapter(food_groups,images,food_group_edit,dialog);
+        adapter = new FoodGroupAdapter(food_groups,images,food_group_edit,dialog,card_view_main);
         food_group_list.setAdapter(adapter);
         dialog.show();
     }
@@ -97,12 +119,50 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int hourFull = timePicker.getHour();
                 int minute = timePicker.getMinute();
-                String timeofDay = hourFull > 12 ? "PM" : "AM";
-                time_edit.setText((hourFull > 12 ? hourFull-12 : hourFull)+":"+minute+" "+ timeofDay);
+                String am_pm = hourFull > 12 ? "PM" : "AM";
+                time_edit.setText((hourFull > 12 ? hourFull-12 : hourFull)+":"+String.format("%02d",minute)+" "+ am_pm);
                 dialog.dismiss();
             }
         });
         dialog.show();
+    }
+
+    private void show_meal_dialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.MyDialogTheme);
+        final ArrayAdapter<String> mealAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.select_dialog_item){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView item = (TextView) view.findViewById(android.R.id.text1);
+                item.setTextColor(Color.WHITE);
+                return view;
+            }
+        };
+        mealAdapter.add("Snack");
+        mealAdapter.add("Breakfast");
+        mealAdapter.add("Lunch");
+        mealAdapter.add("Dinner");
+        mealAdapter.add("Supper");
+        mealAdapter.add("Tea Time");
+        mealAdapter.add("Brunch");
+
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setAdapter(mealAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String meal = mealAdapter.getItem(which);
+                meal_edit.setText(meal);
+                
+            }
+        });
+        builder.show();
     }
 
 }

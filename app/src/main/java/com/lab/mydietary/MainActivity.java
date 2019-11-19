5d5823e89e02d1382de5f05b567ea5452f54c0e0
Eshuen Lam <shuen5035@gmail.com>
2019-11-19 18:29:10 +0800

    Date Time Input added

--- app/src/main/java/com/lab/mydietary/MainActivity.java
+++ app/src/main/java/com/lab/mydietary/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText food_group_edit;
+    private EditText food_group_edit,date_edit,time_edit;
     private RecyclerView food_group_list;
     private FoodGroupAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -20,20 +26,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         food_group_edit = findViewById(R.id.food_group_edit);
+        date_edit = findViewById(R.id.date_edit);
+        time_edit = findViewById(R.id.time_edit);
         food_group_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                show_dialog();
+                show_group_dialog();
+            }
+        });
+        date_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_date_dialog();
+            }
+        });
+        time_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show_time_dialog();
             }
         });
 
     }
 
-    private void show_dialog()
+    private void show_group_dialog()
     {
         food_group_edit = findViewById(R.id.food_group_edit);
         Dialog dialog = new Dialog(MainActivity.this);
-        dialog.setTitle("About Us");
+        dialog.setTitle("Food Group");
         dialog.setContentView(R.layout.dialog_food_group);
         food_group_list =(RecyclerView)dialog.findViewById(R.id.food_group_list);
         layoutManager = new LinearLayoutManager(this);
@@ -45,4 +65,44 @@ public class MainActivity extends AppCompatActivity {
         food_group_list.setAdapter(adapter);
         dialog.show();
     }
+
+    private void show_date_dialog()
+    {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Date Picker");
+        dialog.setContentView(R.layout.dialog_date);
+
+        DatePicker datePicker = (DatePicker) dialog.findViewById(R.id.date_picker);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                date_edit.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void show_time_dialog()
+    {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Time Picker");
+        dialog.setContentView(R.layout.dialog_time);
+        final TimePicker timePicker = (TimePicker) dialog.findViewById(R.id.time_picker);
+        Button set = dialog.findViewById((R.id.set_button));
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hourFull = timePicker.getHour();
+                int minute = timePicker.getMinute();
+                String timeofDay = hourFull > 12 ? "PM" : "AM";
+                time_edit.setText((hourFull > 12 ? hourFull-12 : hourFull)+":"+minute+" "+ timeofDay);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 }
